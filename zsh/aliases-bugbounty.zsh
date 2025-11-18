28943d44f46abb673107a908788c78fa021ea053
theoffsecgirl <arianne@theoffsecgirl.com>
2025-11-18 06:35:56 +0100

    Delete zsh directory

--- zsh/aliases-bugbounty.zsh
+++
@@ -1,5 +0,0 @@
-# Alias orientados a seguridad ofensiva y bug bounty
-
-alias httprobe='httpx -silent'
-alias subenum='subfinder -silent'
-alias ports='nmap -Pn -sV'
--- zsh/aliases-core.zsh
+++
@@ -1,7 +0,0 @@
-# Alias generales del sistema
-
-alias ll='ls -lAh'
-alias la='ls -A'
-alias gs='git status'
-alias gc='git commit'
-alias v='nvim'
