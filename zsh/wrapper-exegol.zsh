2f2b4cf052dcd55bece543cc561d639d0d702d90
theoffsecgirl <arianne@theoffsecgirl.com>
2025-12-22 18:25:28 +0100

    Update wrapper-exegol.zsh

--- zsh/wrapper-exegol.zsh
+++ zsh/wrapper-exegol.zsh
@@ -1,8 +1,15 @@
-# Wrapper para ejecutar herramientas ofensivas dentro de Exegol.
-# Centraliza toda la ejecución de hacking en el contenedor (imagen free),
-# manteniendo el host limpio y evitando dependencias locales.
-#
-# Uso:
-#   ex nmap -sC -sV target.com
-#   ex ffuf -u https://target/FUZZ -w wordlist.txt
-ex() { exegol exec free -- "$@"; }
+# Ejecuta comandos en Exegol (imagen free) y filtra solo el banner de Exegol
+ex() {
+  exegol exec -v free -- "$@" 2>&1 | sed -E '
+    /Exegol Community is currently in version/d
+    /More about Exegol at:/d
+    /Executing command on Exegol/d
+    /Command received:/d
+    /End of the command/d
+  '
+}
+
+# Modo debug (sin filtrar)
+exv() {
+  exegol exec -v free -- "$@"
+}
